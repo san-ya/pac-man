b7c6d7e07b73b76f3b69044e0e47d47a4bc3ecf4
Anshika Gautam <anshikagautam130@gmail.com>
2021-11-22 22:21:06 +0530

    sounds added + new changes in file

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-let grid = document.querySelector(".grid");
-// let pacmanHead = document.querySelectorAll("h1").children;
-// let pacmanChild = pacmanHead.children;
 let pacmanHead = document.getElementsByClassName("pacman-head");
-
-//color
+const startGameAudio = new Audio('./sounds/game_start.wav');
+//colors array
 const colors = ["cyan", "#FFB8FF", "#FFB852", "red"];
 
+//change color of heading
 setInterval(function changeHeadColor() {
   var i;
   for (i = 0; i < pacmanHead.length; i++) {
@@ -14,6 +12,9 @@ setInterval(function changeHeadColor() {
     let pacSpan = pacmanHead[i];
     pacSpan.style.color = randomColor;
   }
-},
-100
-);
+}, 100);
+
+window.onload = function (){
+  startGameAudio.play();
+  // startGameAudio.loop = true;
+}
