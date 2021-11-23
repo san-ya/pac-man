ce2ae26db69f70efa09c5f3cfcdce59cb2c5fd0f
Anshika Gautam <anshikagautam130@gmail.com>
2021-11-23 13:06:00 +0530

    grid size array

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
 let grid = document.querySelector(".grid");
+
+const gridSize = [
+    {
+        level: 'easy',
+        grid: 19
+    },
+    {
+        level: 'medium',
+        grid: 19
+    },
+    {
+        level: 'hard',
+        grid: 38
+    }
+]
