dc3b824ccf0ebcb1fdb395334781caf7eb3525e6
Shivangi32 <sdatisha.2195@gmail.com>
2021-11-23 12:49:02 +0530

    commit

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
 let grid = document.querySelector(".grid");
+
+let rows = 19;
+let cells = 19;
+
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+function makeRows(rowNum) {
+
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement("div");
+        grid.appendChild(row).className = "gridRow";
+    };
+};
+
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+
+    };
+};
