1975e65dccbdbfb62b13cb54a90b3dd12fa88d2b
Shivangi32 <sdatisha.2195@gmail.com>
2021-12-14 15:52:56 +0530

    server created

---
+++ server.js
@@ -0,0 +1,31 @@
+var express=require("express");
+var app=express();
+
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname+"\\Assets\\index.html");
+})
+
+
+app.listen(4000,function(){
+    console.log("Listening to port 4000");
+})
+
+app.get("/Assets/:query",(req,res)=>{
+    res.sendFile(__dirname+"\\Assets\\"+req.params.query);
+    console.log(__dirname+"\\Assets"+req.params.query)
+})
+
+app.get("/sounds/:query",(req,res)=>{
+    res.sendFile(__dirname+"\\sounds\\"+req.params.query);
+    console.log(__dirname+"\\Assets"+req.params.query)
+})
+
+app.get("/login",(req,res)=>{
+    var username=req.body.username;
+    res.sendFile(__dirname+"\\Assets\\game.html");
+  })
